6f83e0700206d39736dee7ffdc297c4052859bd7
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-10-27 18:43:30 +0300

    fix endpoint

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="ca-rent">
+      <BrowserRouter basename="ca-rent-time">
         <App />
         <ToastContainer
           position="bottom-right"
